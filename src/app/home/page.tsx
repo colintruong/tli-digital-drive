56e5383dd390092018ab1ede8925b326e1e7261f
colintruong <117537500+colintruong@users.noreply.github.com>
2026-09-03 14:42:20 -0700

    Updating the github

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -212,8 +212,19 @@ export default function HomePage() {
       <div className="flex flex-col items-center min-h-screen bg-[#fff6f1]">
         <div className="w-2/5">
           <h1>Home</h1>
-          <p>Logged in as: {user?.email}</p>
-          <button onClick={signOut}>Sign out</button>
+          <div className="flex justify-between">
+            <>
+              <p>Logged in as: {user?.email}</p>
+              <button onClick={signOut}>Sign out</button>
+            </>
+
+            <button
+              onClick={() => setIsModalOpen(true)}
+              className="px-4 py-2 rounded-xl bg-[#e97b8e] text-white cursor-pointer"
+            >
+              Upload
+            </button>
+          </div>
 
           <hr />
 
